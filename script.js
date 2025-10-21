afe41f3be872dba9a7614b2c36a0faa4d8b1126f
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2025-10-21 16:50:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -712,7 +712,42 @@ const allSongs = [
         duration: "4:16",
         src: "/hymns/Hymn 269 Glorious Things of Thee Are Spoken.mp3",
         content: `
-                </br> `
+                </br>Glorious things of thee are spoken,
+                    </br>Zion, city of our God;
+                    </br>He whose word cannot be broken
+                    </br>Formed thee for his own abode:
+                    </br>On the Rock of Ages founded,
+                    </br>What can shake thy sure repose?
+                    </br>With salvation's walls surrounded,
+                    </br>Thou may'st smile at all thy foes.
+</br>
+                    </br>See the streams of living waters,
+                    </br>Springing from eternal love,
+                    </br>Well supply thy sons and daughters,
+                    </br>And all fear of want remove;
+                    </br>Who can faint, while such a river
+                    </br>Ever flows their thirst t'assuage?
+                    </br>Grace which, like the Lord, the giver,
+                    </br>Never fails from age to age.
+</br>
+                    </br>Round each habitation hov'ring,
+                    </br>See the cloud and fire appear
+                    </br>For a glory and a cov'ring,
+                    </br>Showing that the Lord is near:
+                    </br>Thus deriving from their banner
+                    </br>Light by night and shade by day,
+                    </br>Safe they feed upon the manna
+                    </br>Which he gives them when they pray.
+</br>
+                    </br>Saviour, if of Zion's city
+                    </br>I, through grace, a member am,
+                    </br>Let the world deride or pity,
+                    </br>I will glory in thy Name:
+                    </br>Fading is the worldling's pleasure,
+                    </br>All his boasted pomp and show;
+                    </br>Solid joys and lasting treasure
+                    </br>None but Zion's children know.
+                 `
     }   
 
         
